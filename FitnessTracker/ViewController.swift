f6df8a06af14d01f73b5bbf7a38a69da665f7f6b
Vadim Gamzaev <Vadosbox@gmail.com>
2023-03-09 00:23:37 +0700

    Initial Commit

---
+++ FitnessTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessTracker
+//
+//  Created by Вадим Гамзаев on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
